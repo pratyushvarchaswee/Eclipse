b5248c46b1f364babc00890df75faca5a13d7212
pratyushvarchaswee <pratyushvarchaswee@gmail.com>
2016-12-06 22:28:44 -0500

    Added Comment and problem description

--- com.homestuffs.spring/src/main/java/codingprobs/BalancePoint.java
+++ com.homestuffs.spring/src/main/java/codingprobs/BalancePoint.java
@@ -1,31 +1,35 @@
 package codingprobs;
 
+/**
+ * It would return a array index for which sum of left side of element from index and sum of
+ * right side of  element from index is equal.
+ * 
+ * @author pratyushvarchaswee
+ *
+ */
 public class BalancePoint {
 
+	public static void main(String[] args) {
 
+		int[] a = { 2, 7, 4, 5, -3, 8, 9, -1 };
 
-	
-	public static void main(String []args){
-	       
-	       int[] a = {2, 7, 4, 5, -3, 8, 9, -1};
-	       
-	        System.out.println("Balance point for a is index "+indexPoint(a));
-	     }
-	     public static int indexPoint(int[] input)
-	    {
-	    		int leftSum = input[0];
-	    		int rightSum = 0;;
-	    		for(int i=0; i<input.length;i++)
-	    			rightSum += input[i];
-	    		
-	    		for(int i=0; i<input.length-1;i++)
-	    		{
-	    			if(leftSum==rightSum)
-	    				return i;
-	    			leftSum+=input[i+1];
-	    			rightSum-=input[i];
-	    		}
-	    		return -1;
+		System.out.println("Balance point for a is index " + indexPoint(a));
+	}
+
+	public static int indexPoint(int[] input) {
+		int leftSum = input[0];
+		int rightSum = 0;
+		;
+		for (int i = 0; i < input.length; i++)
+			rightSum += input[i];
+
+		for (int i = 0; i < input.length - 1; i++) {
+			if (leftSum == rightSum)
+				return i;
+			leftSum += input[i + 1];
+			rightSum -= input[i];
 		}
+		return -1;
+	}
 
 }
--- com.homestuffs.spring/src/main/java/codingprobs/DivisorProb.java
+++ com.homestuffs.spring/src/main/java/codingprobs/DivisorProb.java
@@ -1,76 +1,72 @@
 package codingprobs;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 
+/**
+ * Kristen loves playing with and comparing numbers. She thinks that if she
+ * takes two different positive numbers, the one whose digits sum to a larger
+ * number is better than the other. If the sum of digits is equal for both
+ * numbers, then she thinks the smaller number is better. For example, Kristen
+ * thinks that is better than and that is better than .
+ * 
+ * @author pratyushvarchaswee
+ *
+ */
 public class DivisorProb {
 
 	public static void main(String[] args) {
-		
-		System.out.println("i/p ur number");
-		 Scanner in = new Scanner(System.in);
-	        int n = in.nextInt();
-	        Map<Integer,Integer> mapInt= new TreeMap<>();
-	       
-	       for(int i=1;i<=n;i++)
-	           {
-	           if(n%i==0)
-	               {
-	        	    
-	            
-	             int  temp=sumDigit(i);
-	               mapInt.put(Integer.valueOf(i), Integer.valueOf(temp));
-	             
-	           }
-	           else
-	               continue;
-	       }
-	       System.out.println(mapInt);
-	    
-	       Set<Integer> keys=mapInt.keySet();
-	       System.out.println(favNum(keys,mapInt));
-	       
-	    
-	    }
 
-	
-	public static int sumDigit(int num)
-	{
-		int digit=0;
+		System.out.println("i/p ur number");
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		Map<Integer, Integer> mapInt = new TreeMap<>();
+
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+
+				int temp = sumDigit(i);
+				mapInt.put(Integer.valueOf(i), Integer.valueOf(temp));
+
+			} else
+				continue;
+		}
+		System.out.println(mapInt);
+
+		Set<Integer> keys = mapInt.keySet();
+		System.out.println(favNum(keys, mapInt));
+
+	}
+
+	public static int sumDigit(int num) {
+		int digit = 0;
 		for (char ch : Integer.toString(num).toCharArray()) {
-		    digit = digit+ch - '0';
-		   
+			digit = digit + ch - '0';
+
 		}
 
 		return digit;
 	}
-	
-	public static int favNum(Set<Integer>numKeys,Map<Integer,Integer> numMap)
-	{
-		Iterator<Integer> itr=numKeys.iterator();
-		
-		int fav=0;
-		int favKey=0;
-		
-		while(itr.hasNext())
-		{
-			Integer key=itr.next();
-			int val=numMap.get(key);
-			if(val>fav)
-			{
-			fav=val;
-			favKey=key;
+
+	public static int favNum(Set<Integer> numKeys, Map<Integer, Integer> numMap) {
+		Iterator<Integer> itr = numKeys.iterator();
+
+		int fav = 0;
+		int favKey = 0;
+
+		while (itr.hasNext()) {
+			Integer key = itr.next();
+			int val = numMap.get(key);
+			if (val > fav) {
+				fav = val;
+				favKey = key;
 			}
-			
+
 		}
 		return favKey;
 	}
-	
-	}
 
-
+}
